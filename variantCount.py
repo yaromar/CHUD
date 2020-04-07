0c7e7f4292b231b96198e27842afb59aca38f06c
Yaro <22304756+yaromar@users.noreply.github.com>
2020-04-07 00:03:03 -0400

    added comments

--- variantCount.py
+++ variantCount.py
@@ -9,6 +9,7 @@ annotations = annotations.fillna({'SOMATIC': '0'})
 #the FOLDER with the somatic calls files should be in the current directory
 #this folder's name should be 'Filtered_SomaticCalls_v1'
 #the files should be compressed and have .bgz extension
+#there should be no other files
 with open('variantCount_output.txt', 'w') as fh:
     fh.write('\t'.join(['sample_id', 'num_FilterMutect', 'annotated_overlap', 'SOMATIC', 'LeukemiaGene', 'TOPMed_CHIPVar', '\n']))
     
