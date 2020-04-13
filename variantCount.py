b8638ea1a2edb58ca9326b98a055875bc59c8c1d
Yaro <22304756+yaromar@users.noreply.github.com>
2020-04-13 19:28:06 -0400

    Update variantCount.py
    fixed the header and the tabs

--- variantCount.py
+++ variantCount.py
@@ -10,13 +10,15 @@ annotations = annotations.fillna({'SOMATIC': '0'})
 #this folder's name should be 'Filtered_SomaticCalls_v1'
 #the files should be compressed and have .bgz extension
 #there should be no other files
-with open('variantCount_output.txt', 'w') as fh:
-    fh.write('\t'.join(['sample_id', 'num_FilterMutect', 'annotated_overlap', 'SOMATIC', 'LeukemiaGene', 'TOPMed_CHIPVar', '\n']))
-    
+with open('test.txt', 'w') as fh:
+    fh.write('\t'.join(['sample_id', 'num_FilterMutect', 'annotated_overlap', 'LeukemiaGene', 'TOPMed_CHIPVar', 'SOMATIC']))
+    fh.write('\n')
+
     for file in listdir('Filtered_SomaticCalls_v1'):
         sample = pd.read_csv('Filtered_SomaticCalls_v1/'+file, delimiter='\t', compression='gzip', usecols=[0]).set_index('v')
         temp = pd.concat([annotations, sample], axis=1, join='inner')
         sampleID, mutectVar = [file.split('.', 4)[i] for i in (0, 3)]
         sampleID = sampleID.split('.', 4)[0].split('-')[0]
 
-        fh.write('\t'.join([sampleID, mutectVar, str(len(pd.concat([annotations, sample], axis=1, join='inner').index)), str(len(temp.loc[temp['LeukemiaGene'] == 1].index)), str(len(temp.loc[pd.notna(temp['TOPMed_CHIPVar'])].index)), str(len(temp.loc[temp['SOMATIC'].str.contains('1')])), '\n']))
+        fh.write('\t'.join([sampleID, mutectVar, str(len(pd.concat([annotations, sample], axis=1, join='inner'))), str(len(temp.loc[temp['LeukemiaGene'] == 1])), str(len(temp.loc[pd.notna(temp['TOPMed_CHIPVar'])])), str(len(temp.loc[temp['SOMATIC'].str.contains('1')]))]))
+        fh.write('\n')
