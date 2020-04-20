22fc240791b14790c0e61d707f2fbc96d6889845
Yaro <22304756+yaromar@users.noreply.github.com>
2020-04-20 15:00:19 -0400

    Update variantCount.py
    all 16 counts, and some comments

--- variantCount.py
+++ variantCount.py
@@ -11,14 +11,58 @@ annotations = annotations.fillna({'SOMATIC': '0'})
 #the files should be compressed and have .bgz extension
 #there should be no other files
 with open('test.txt', 'w') as fh:
-    fh.write('\t'.join(['sample_id', 'num_FilterMutect', 'annotated_overlap', 'LeukemiaGene', 'TOPMed_CHIPVar', 'SOMATIC']))
+    fh.write('\t'.join(['sample_id',
+                        'num_FilterMutect',
+                        'overlap',
+                        'overlap_&_LeukemiaGene',
+                        'overlap_&_TOPMed_CHIPVar',
+                        'overlap_&_SOMATIC',
+                        ####
+                        'overlap_&_binom',
+                        'overlap_&_LeukemiaGene_&_binom',
+                        'overlap_&_TOPMed_CHIPVar_&_binom',
+                        'overlap_&_SOMATIC_&_binom',
+                        ####
+                        'overlap_&_VAF',
+                        'overlap_&_LeukemiaGene_&_VAF',
+                        'overlap_&_TOPMed_CHIPVar_&_VAF',
+                        'overlap_&_SOMATIC_&_VAF',
+                        ####
+                        'overlap_&_binom_&_VAF',
+                        'overlap_&_LeukemiaGene_&_binom_&_VAF',
+                        'overlap_&_TOPMed_CHIPVar_&_binom_&_VAF',
+                        'overlap_&_SOMATIC_&_binom_&_VAF',
+                       ]))
     fh.write('\n')
 
     for file in listdir('Filtered_SomaticCalls_v1'):
-        sample = pd.read_csv('Filtered_SomaticCalls_v1/'+file, delimiter='\t', compression='gzip', usecols=[0]).set_index('v')
+        sample = pd.read_csv('Filtered_SomaticCalls_v1/1169275_23173_0_0-filtered.vcf.gz.1577.GTs.bgz', delimiter='\t', compression='gzip', usecols=["v", "Binomial_Prob", "VAF"]).set_index('v')
         temp = pd.concat([annotations, sample], axis=1, join='inner')
+
         sampleID, mutectVar = [file.split('.', 4)[i] for i in (0, 3)]
         sampleID = sampleID.split('.', 4)[0].split('-')[0]
 
-        fh.write('\t'.join([sampleID, mutectVar, str(len(pd.concat([annotations, sample], axis=1, join='inner'))), str(len(temp.loc[temp['LeukemiaGene'] == 1])), str(len(temp.loc[pd.notna(temp['TOPMed_CHIPVar'])])), str(len(temp.loc[temp['SOMATIC'].str.contains('1')]))]))
+
+        fh.write('\t'.join([sampleID,
+                            mutectVar,
+                            str(len(pd.concat([annotations, sample], axis=1, join='inner'))), #overlap
+                            str(len(temp.loc[temp['LeukemiaGene'] == 1])), #overlap & leukemia
+                            str(len(temp.loc[pd.notna(temp['TOPMed_CHIPVar'])])), #overlap & topmed
+                            str(len(temp.loc[temp['SOMATIC'].str.contains('1')])), #overlap & somatic literature
+                            #####
+                            str(len(temp.query('Binomial_Prob < 0.01'))), #overlap & binom
+                            str(len(temp.query('Binomial_Prob < 0.01 & LeukemiaGene == 1'))), #overlap & leukemia & binom
+                            str(len(temp.loc[pd.isna(temp['TOPMed_CHIPVar'])].query('Binomial_Prob < 0.01'))), #overlap & topmed & binom
+                            str(len(temp.loc[temp['SOMATIC'].str.contains('1')].query('Binomial_Prob < 0.01'))), #overlap & somatic literature & binomial
+                            #####
+                            str(len(temp.query('VAF > 0.1'))), #overlap & VAF
+                            str(len(temp.query('VAF > 0.1 & LeukemiaGene == 1'))), #overlap & leukemia & VAF
+                            str(len(temp.loc[pd.isna(temp['TOPMed_CHIPVar'])].query('VAF > 0.1'))), #overlap & topmed & VAF
+                            str(len(temp.loc[temp['SOMATIC'].str.contains('1')].query('VAF > 0.1'))), #overlap & somatic literature & VAF
+                            #####
+                            str(len(temp.query('Binomial_Prob < 0.01 & VAF > 0.1'))), #overlap & binom
+                            str(len(temp.query('Binomial_Prob < 0.01 & VAF > 0.1 & LeukemiaGene == 1'))), #overlap & leukemia & binom & VAF
+                            str(len(temp.loc[pd.isna(temp['TOPMed_CHIPVar'])].query('Binomial_Prob < 0.01 & VAF > 0.1'))), #overlap & topmed & binom & VAF
+                            str(len(temp.loc[temp['SOMATIC'].str.contains('1')].query('Binomial_Prob < 0.01 & VAF > 0.1'))) #overlap & somatic literature & binomial & VAF
+                           ]))
         fh.write('\n')
